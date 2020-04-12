dc5278b5e893eff51871e6ebfa0873f517c12121
Ridwan Andalus <ridwan.andalus79@gmail.com>
2020-04-12 21:51:19 +0700

    Change about page and blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -34,7 +34,7 @@
 				<div class="container">
 					<div class="row align-items-center justify-content-between d-flex">
 						<div id="logo">
-							<a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+							<a href="{{ route('home')}}"><img src="img/logo.png" alt="" title="" /></a>
 						</div>
 						<nav id="nav-menu-container">
 							<ul class="nav-menu">
@@ -67,7 +67,7 @@
 							<h1 class="text-white">
 								Blog				
 							</h1>	
-							<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="blog-home.html"> Blog </a></p>
+							<p class="text-white link-nav"><a href="{{ route('home')}}">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="{{ route('blog-home')}}"> Blog </a></p>
 						</div>											
 					</div>
 				</div>
@@ -80,20 +80,19 @@
 					<div class="row">
 						<div class="col-lg-8 post-list blog-post-list">
 							<div class="single-post">
-								<img class="img-fluid" src="img/blog/p1.jpg" alt="">
+								<img class="img-fluid" src="img/mobil/expander.jpg" alt="" class="img-fluid">
 								<ul class="tags">
-									<li><a href="#">Art, </a></li>
-									<li><a href="#">Technology, </a></li>
-									<li><a href="#">Fashion</a></li>
+									<li><a href="#">Car, </a></li>
+									<li><a href="#">Sport, </a></li>
+									<li><a href="#">Automotif</a></li>
 								</ul>
-								<a href="blog-single.html">
+								<a href="{{ route('blog-home')}}">
 									<h1>
-										Cartridge Is Better Than Ever
-										A Discount Toner
+										Suzuki Expander
 									</h1>
 								</a>
 								<p>
-									MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+									Mobil ini banyak peminatnya loh, gahanya membeli, tetapi banyak juga yang merental mobil ini di toko rental yang sudah kami informasikan.
 								</p>
 								<div class="bottom-meta">
 									<div class="user-details row align-items-center">
@@ -116,27 +115,26 @@
 								</div>
 							</div>
 							<div class="single-post">
-								<img class="img-fluid" src="img/blog/p2.jpg" alt="">
+								<img class="img-fluid" src="img/mobil/fortuner.jpg" alt="" class="img-fluid">
 								<ul class="tags">
 									<li><a href="#">Art, </a></li>
-									<li><a href="#">Technology, </a></li>
-									<li><a href="#">Fashion</a></li>
+									<li><a href="#">Sport, </a></li>
+									<li><a href="#">Automotif</a></li>
 								</ul>
-								<a href="blog-single.html">
+								<a href="{{ route('blog-home')}}">
 									<h1>
-										Cartridge Is Better Than Ever
-										A Discount Toner
+										Fortuner
 									</h1>
 								</a>
 								<p>
-									MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+									Mobil ini, ialah pesaing dari Mitsubitsi Pajero, dan salah satu mobil yang paling banyak di sukai di indonesia.
 								</p>
 								<div class="bottom-meta">
 									<div class="user-details row align-items-center">
 										<div class="comment-wrap col-lg-6">
 											<ul>
-												<li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-												<li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
+												<li><a href="#"><span class="lnr lnr-heart"></span>	5 likes</a></li>
+												<li><a href="#"><span class="lnr lnr-bubble"></span> 10 Comments</a></li>
 											</ul>
 										</div>
 										<div class="social-wrap col-lg-6">
@@ -151,78 +149,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="single-post">
-								<img class="img-fluid" src="img/blog/p3.jpg" alt="">
-								<ul class="tags">
-									<li><a href="#">Art, </a></li>
-									<li><a href="#">Technology, </a></li>
-									<li><a href="#">Fashion</a></li>
-								</ul>
-								<a href="blog-single.html">
-									<h1>
-										Cartridge Is Better Than Ever
-										A Discount Toner
-									</h1>
-								</a>
-								<p>
-									MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
-								</p>
-								<div class="bottom-meta">
-									<div class="user-details row align-items-center">
-										<div class="comment-wrap col-lg-6">
-											<ul>
-												<li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-												<li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
-											</ul>
-										</div>
-										<div class="social-wrap col-lg-6">
-											<ul>
-												<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-												<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-												<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-												<li><a href="#"><i class="fa fa-behance"></i></a></li>
-											</ul>
-											
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="single-post">
-								<img class="img-fluid" src="img/blog/p4.jpg" alt="">
-								<ul class="tags">
-									<li><a href="#">Art, </a></li>
-									<li><a href="#">Technology, </a></li>
-									<li><a href="#">Fashion</a></li>
-								</ul>
-								<a href="blog-single.html">
-									<h1>
-										Cartridge Is Better Than Ever
-										A Discount Toner
-									</h1>
-								</a>
-								<p>
-									MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
-								</p>
-								<div class="bottom-meta">
-									<div class="user-details row align-items-center">
-										<div class="comment-wrap col-lg-6">
-											<ul>
-												<li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-												<li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
-											</ul>
-										</div>
-										<div class="social-wrap col-lg-6">
-											<ul>
-												<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-												<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-												<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-												<li><a href="#"><i class="fa fa-behance"></i></a></li>
-											</ul>
-											
-										</div>
-									</div>
-								</div>
-							</div>																					
+
 						</div>
 						<div class="col-lg-4 sidebar">
 							<div class="single-widget search-widget">
@@ -233,12 +160,10 @@
 							</div>
 
 							<div class="single-widget protfolio-widget">
-								<img src="img/blog/user2.jpg" alt="">
-								<a href="#"><h4>Adele Gonzalez</h4></a>
+								<img src="img/keken.jpeg" alt="" class="img-fluid">
+								<a href="#"><h4>Website Informasi Tentang Toko Rental</h4></a>
 								<p>
-									MCSE boot camps have its supporters and
-									its detractors. Some people do not understand why you should have to spend money
-									on boot camp when you can get.
+									Semua informasi seputar toko rental mobil, sangat benar sekali bila anda melihat website ini dan mencari toko rental yang kalian inginkan.
 								</p>
 								<ul>
 									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -258,104 +183,6 @@
 									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Food</h6> <span>15</span></a></li>
 									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Architecture</h6> <span>09</span></a></li>
 									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Adventure</h6> <span>44</span></a></li>
-								</ul>
-							</div>
-
-							<div class="single-widget recent-posts-widget">
-								<h4 class="title">Recent Posts</h4>
-								<div class="blog-list ">
-									<div class="single-recent-post d-flex flex-row">
-										<div class="recent-thumb">
-											<img class="img-fluid" src="img/blog/r1.jpg" alt="">
-										</div>
-										<div class="recent-details">
-											<a href="blog-single.html">
-												<h4>
-													Home Audio Recording
-													For Everyone
-												</h4>
-											</a>
-											<p>
-												02 hours ago
-											</p>
-										</div>
-									</div>	
-									<div class="single-recent-post d-flex flex-row">
-										<div class="recent-thumb">
-											<img class="img-fluid" src="img/blog/r2.jpg" alt="">
-										</div>
-										<div class="recent-details">
-											<a href="blog-single.html">
-												<h4>
-													Home Audio Recording
-													For Everyone
-												</h4>
-											</a>
-											<p>
-												02 hours ago
-											</p>
-										</div>
-									</div>	
-									<div class="single-recent-post d-flex flex-row">
-										<div class="recent-thumb">
-											<img class="img-fluid" src="img/blog/r3.jpg" alt="">
-										</div>
-										<div class="recent-details">
-											<a href="blog-single.html">
-												<h4>
-													Home Audio Recording
-													For Everyone
-												</h4>
-											</a>
-											<p>
-												02 hours ago
-											</p>
-										</div>
-									</div>	
-									<div class="single-recent-post d-flex flex-row">
-										<div class="recent-thumb">
-											<img class="img-fluid" src="img/blog/r4.jpg" alt="">
-										</div>
-										<div class="recent-details">
-											<a href="blog-single.html">
-												<h4>
-													Home Audio Recording
-													For Everyone
-												</h4>
-											</a>
-											<p>
-												02 hours ago
-											</p>
-										</div>
-									</div>																																					
-								</div>								
-							</div>
-
-							<div class="single-widget category-widget">
-								<h4 class="title">Post Archive</h4>
-								<ul>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Dec '17</h6> <span>37</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Nov '17</h6> <span>24</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Oct '17</h6> <span>59</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Sep '17</h6> <span>29</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Aug '17</h6> <span>15</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Jul '17</h6> <span>09</span></a></li>
-									<li><a href="#" class="justify-content-between align-items-center d-flex"><h6>Jun '17</h6> <span>44</span></a></li>
-								</ul>
-							</div>			
-
-							<div class="single-widget tags-widget">
-								<h4 class="title">Tag Clouds</h4>
-								<ul>
-									<li><a href="#">Lifestyle</a></li>
-									<li><a href="#">Art</a></li>
-									<li><a href="#">Adventure</a></li>
-									<li><a href="#">Food</a></li>
-									<li><a href="#">Techlology</a></li>
-									<li><a href="#">Fashion</a></li>
-									<li><a href="#">Architecture</a></li>
-									<li><a href="#">Food</a></li>
-									<li><a href="#">Technology</a></li>
 								</ul>
 							</div>				
 
